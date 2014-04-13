af56bfd4005224e582d64cfaf78048175dc853c3
ssgreg <me@ssgreg.com>
2014-04-13 12:03:19 +0400

    Initial Commit

---
+++ mp4_metadata_extractor/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mp4_metadata_extractor
+//
+//  Created by Grigory Zubankov on 13/04/14.
+//  Copyright (c) 2014 Grigory Zubankov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
